6bf2578d3df0287a18bbd721d90c24c55baf3965
Felix <fxp.8sam@icloud.com>
2024-03-24 18:36:44 +0100

    improved cacheManager

--- bot/database/cacheManager.py
+++ bot/database/cacheManager.py
@@ -25,16 +25,12 @@ class CacheManager:
         self.bot = bot
 
     @lru_cache(maxsize=128, typed=False)
-    async def cached_dbselect(self, column: str, guild_id: int):
-        res = await self.bot.db.field(f"SELECT {column} FROM {self.bot.db.schema}.system WHERE GUILD_ID = {guild_id}")
+    async def cached_dbselect(self, query):
+        res = await self.bot.db.field(query)
         return res
     
-    async def set_dbupdate(self, column: str, value: int, guild_id: int):
-        await self.bot.db.execute(f"UPDATE {self.bot.db.schema}.system SET {column} = {value} WHERE GUILD_ID = {guild_id}")
-        self.cached_dbselect.cache_clear()
-    
-    async def insert_dbrecord(self, column: str, value: int, guild_id: int):
-        await self.bot.db.execute(f"INSERT INTO {self.bot.db.schema}.system ({column}, GUILD_ID) VALUES ({value}, {guild_id})")
+    async def execute_update(self, query):
+        await self.bot.db.execute(query)
         self.cached_dbselect.cache_clear()
 
 
